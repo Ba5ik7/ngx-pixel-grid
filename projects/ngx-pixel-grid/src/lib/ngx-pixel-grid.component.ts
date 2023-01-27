32a6f6561ff5cdb90177914b8a2da564474102e6
Wesley DuSell <dusellfam@gmail.com>
2023-01-27 11:03:20 -0500

    Add canvas and wrapper/container tags and reference them with ViewChild.

--- projects/ngx-pixel-grid/src/lib/ngx-pixel-grid.component.ts
+++ projects/ngx-pixel-grid/src/lib/ngx-pixel-grid.component.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'ngx-pixel-grid',
   template: `
-    <p>
-      ngx-pixel-grid works!
-    </p>
-  `,
-  styles: [
-  ]
+  <div #pixelGridCanvasContatiner class="pixel-grid-canvas-container">
+    <canvas #pixelGridCanvas></canvas>
+  </div>`,
+  styles: ['.pixel-grid-canvas-container { width: 100%; height: 100%; }']
 })
-export class NgxPixelGridComponent implements OnInit {
+export class NgxPixelGridComponent implements AfterViewInit {
+
+  @ViewChild('pixelGridCanvasContatiner') pixelGridCanvasContatiner!: HTMLDivElement;
+  @ViewChild('pixelGridCanvas') pixelGridCanvas!: HTMLCanvasElement;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngAfterViewInit(): void {
 
+  }
 }
