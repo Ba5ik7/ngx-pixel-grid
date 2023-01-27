6532f8807bbb7c10baa320be7ea623625b19e830
Wesley DuSell <dusellfam@gmail.com>
2023-01-27 17:09:31 -0500

    Add inject token for grid options

--- projects/ngx-pixel-grid/src/lib/interfaces/ngx-pixel-grid.ts
+++ projects/ngx-pixel-grid/src/lib/interfaces/ngx-pixel-grid.ts
@@ -1,3 +1,17 @@
+// Make a interface for the options of the pixel grid
+// This interface will be used in the constructor of the PixelGrid class
+// add a option for an intro anmation
+// add a option for the gutter
+// add a option for the rows
+// add a option for the columns
+
+export interface IPixelGridOptions {
+  introAnimation: boolean;
+  gutter: number;
+  rows: number;
+  columns: number;
+}
+
 export interface IPixelGrid {
   gutter: number;
   rows: number;
--- projects/ngx-pixel-grid/src/lib/ngx-pixel-grid.service.ts
+++ projects/ngx-pixel-grid/src/lib/ngx-pixel-grid.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, InjectionToken, Optional } from '@angular/core';
+import { IPixelGridOptions } from './interfaces/ngx-pixel-grid';
 
+export const NGX_PIXEL_GRID_OPTIONS = new InjectionToken<IPixelGridOptions>('NGX_PIXEL_GRID_OPTIONS');
 @Injectable({
   providedIn: 'root'
 })
 export class NgxPixelGridService {
 
-  constructor() { }
+  constructor(@Optional() @Inject(NGX_PIXEL_GRID_OPTIONS) options: IPixelGridOptions) {
+    console.log({ options });
+  }
 }
