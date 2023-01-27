c75206fcb48766e6da2a4b32fe57071c951cda1b
Wesley DuSell <dusellfam@gmail.com>
2023-01-27 18:38:02 -0500

    remove logs

--- projects/ngx-pixel-grid/src/lib/ngx-pixel-grid.component.ts
+++ projects/ngx-pixel-grid/src/lib/ngx-pixel-grid.component.ts
@@ -38,8 +38,6 @@ export class NgxPixelGridComponent implements AfterViewInit {
   @HostListener('window:resize')
   onResize() {
     const pixelGridSize = this.getPixelGridSize(this.pixelGridTilesMatrix, this.pixelGrid.gutter);
-    console.log(pixelGridSize);
-    
     this.pixelGridCanvas.nativeElement.width = pixelGridSize.width;
     this.pixelGridCanvas.nativeElement.height = pixelGridSize.height;
   }
