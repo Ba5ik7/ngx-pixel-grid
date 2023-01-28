91240d75844ae88e5fb57838aa1a49004e336733
Wesley DuSell <dusellfam@gmail.com>
2023-01-28 14:54:45 -0500

    gh-pages stuff

--- dist/ngx-pixel-grid/index.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="@tmdjr/ngx-pixel-grid" />
-export * from './public-api';
-//# sourceMappingURL=tmdjr-ngx-pixel-grid.d.ts.map
--- dist/ngx-pixel-grid/public-api.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/ngx-pixel-grid.service';
-export * from './lib/ngx-pixel-grid.component';
-export * from './lib/ngx-pixel-grid.module';
-export * from './lib/interfaces/ngx-pixel-grid';
-//# sourceMappingURL=public-api.d.ts.map
